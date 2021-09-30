c19308e72470c56aa699b7e1f4bae3d2d08726b7
sss193 <sunny4get@gmail.com>
2021-09-30 22:15:29 +0530

    trial Commit 2

--- app/src/main/java/com/example/localtry/MainActivity.kt
+++ app/src/main/java/com/example/localtry/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ////  komal ..cs,d.s,d.,s.d,s.mdsmd
     }
 }
